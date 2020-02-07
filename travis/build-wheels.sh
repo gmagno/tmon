6b55b2ca5d4fc42bec60edca020407911c2f62cd
Goncalo <goncalo.magno@gmail.com>
2020-02-06 22:03:39 -0300

    Add CI with travis

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -e -x
 
-export PYHOME=/home
+export PYHOME=/io/
 cd ${PYHOME}
 
-/opt/python/cp37-cp37m/bin/pip install twine cmake
-ln -s /opt/python/cp37-cp37m/bin/cmake /usr/bin/cmake
+ln -s /opt/python/cp35-cp35m/bin/python /usr/local/bin/python3.5
+ln -s /opt/python/cp36-cp36m/bin/python /usr/local/bin/python3.6
+ln -s /opt/python/cp37-cp37m/bin/python /usr/local/bin/python3.7
+ln -s /opt/python/cp38-cp38/bin/python /usr/local/bin/python3.8
+
+python3.8 -m pip install -U pip
+python3.8 -m pip install -r requirements_dev.txt
+python3.8 -m pip install twine
 
 # Compile wheels
-for PYBIN in /opt/python/cp3*/bin; do
+for PYBIN in /opt/python/cp3{5,6,7,8}*/bin; do
+    "${PYBIN}/pip" install -U pip
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
     "${PYBIN}/python" /io/setup.py sdist -d /io/wheelhouse/
 done
@@ -19,22 +26,18 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Test
-for PYBIN in /opt/python/cp3*/bin/; do
-    "${PYBIN}/pip" install -r /io/requirements-test.txt
-    "${PYBIN}/pip" install --no-index -f /io/wheelhouse nb-cpp
-    (cd "$PYHOME"; "${PYBIN}/pytest" /io/test/)
-done
+python3.8 -m tox
 
 #  Upload
-for WHEEL in /io/wheelhouse/nb_cpp*; do
+for WHEEL in /io/wheelhouse/tmon*; do
     # dev
-    # /opt/python/cp37-cp37m/bin/twine upload \
+    # /opt/python/cp38-cp38m/bin/twine upload \
     #     --skip-existing \
     #     --repository-url https://test.pypi.org/legacy/ \
     #     -u "${TWINE_USERNAME}" -p "${TWINE_PASSWORD}" \
     #     "${WHEEL}"
     # prod
-    /opt/python/cp37-cp37m/bin/twine upload \
+    /opt/python/cp38-cp38m/bin/twine upload \
         --skip-existing \
         -u "${TWINE_USERNAME}" -p "${TWINE_PASSWORD}" \
         "${WHEEL}"
