6e2eb2d3b77ea487d22637b13be4f19f6966d0ed
Goncalo <goncalo.magno@gmail.com>
2020-02-22 02:14:35 +0000

    - Add support for fahrenheit - Add support for chart/stats/path only report - Detect OS

--- tmon/cli.py
+++ tmon/cli.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
+
 """CLI script for tmon.
 """
 
 import argparse
+import platform
 import textwrap
 import sys
 
@@ -19,16 +22,25 @@ class AxisSizeAction(argparse.Action):
 
 
 def parse_args():
-    description = (
-        "Temperature Monitor (tmon v0.3.8) -- executes a program while\n"
-        "monitoring CPU temperature, reporting the min, max and mean\n"
-        "temperatures and plotting an ascii chart at the end to stderr.\n"
-        "All signals are redirected to the program.\n"
-        "If no program is passed, %(prog)s runs as expected returning on\n"
-        "SIGINT (Ctrl-C).\n"
-        "For full documentation check the repo: https://github.com/gmagno/tmon"
-    )
+    description = textwrap.dedent("""
+        Temperature Monitor (tmon v0.3.8) -- executes a program while
+        monitoring CPU temperature, reporting the min, max and mean
+        temperatures and plotting an ascii chart at the end to stderr.
+        All signals are redirected to the program.
+        If no program is passed, %(prog)s runs as expected returning on
+        SIGINT (Ctrl-C).
+        For full documentation check the repo: https://github.com/gmagno/tmon
+    """)
     epilog = textwrap.dedent("""
+                    /##
+                   | ##
+                  /######   /######/####   /######  /#######
+                 |_  ##_/  | ##_  ##_  ## /##__  ##| ##__  ##
+                   | ##    | ## \ ## \ ##| ##  \ ##| ##  \ ##
+                   | ## /##| ## | ## | ##| ##  | ##| ##  | ##
+                   |  ####/| ## | ## | ##|  ######/| ##  | ##
+                    \___/  |__/ |__/ |__/ \______/ |__/  |__/
+
         return:
             %(prog)s returns when the child program exits, stops, or is
             terminated by a signal. The return value of %(prog)s is the return
@@ -38,84 +50,93 @@ def parse_args():
             $ %(prog)s echo How can a clam cram in a clean cream can
             How can a clam cram in a clean cream can
 
-
             ===================
-            Temp Monitor Report:
+            Temp Monitor Report
 
-            Temp (°C) for a period of 0:00:00
-            >> 53.0 °C <<
+                min: 51.0 °C
+                avg: 51.0 °C
+                max: 51.0 °C
 
-            /tmp/tmon-YYYYMMDD@HHhMMmSS-XXXXXXXX.txt
+                raw: /tmp/tmon-YYYYMMDD@HHhMMmSS-XXXXXXXX.txt
             ===================
 
             $ %(prog)s bash -c 'sleep 6; stress -c 4 -t 3; sleep 6'
-            stress: info: [17832] dispatching hogs: 4 cpu, 0 io, 0 vm, 0 hdd
-            stress: info: [17832] successful run completed in 3s
-
-
-            ===================
-            Temp Monitor Report:
-
-            Temp (°C) for a period of 0:00:15
-            59.00  ┤
-            58.67  ┤
-            58.33  ┤
-            58.00  ┤
-            57.67  ┤      ╭─╮
-            57.33  ┤      │ │
-            57.00  ┤      │ │
-            56.67  ┤     ╭╯ ╰╮
-            56.33  ┤     │   │
-            56.00  ┤     │   │
-            55.67  ┼─╮   │   │
-            55.33  ┤ │   │   │
-            55.00  ┤ ╰───╯   ╰────╮
-            54.67  ┤              │
-            54.33  ┤              │
-            54.00  ┤              ╰
-
-            /tmp/tmon-YYYYMMDD@HHhMMmSS-XXXXXXXX.txt
-            ===================
-
-            $ %(prog)s -y 5 -x 5 bash -c 'sleep 6; stress -c 4 -t 3; sleep 6'
-            stress: info: [17181] dispatching hogs: 4 cpu, 0 io, 0 vm, 0 hdd
-            stress: info: [17181] successful run completed in 3s
-
+            stress: info: [30357] dispatching hogs: 4 cpu, 0 io, 0 vm, 0 hdd
+            stress: info: [30357] successful run completed in 3s
 
             ===================
-            Temp Monitor Report:
+            Temp Monitor Report
 
-            Temp (°C) for a period of 0:00:15
-            60.00  ┤
-            59.00  ┤  ╭╮
-            58.00  ┼╮╭╯│
-            57.00  ┤╰╯ │
-            56.00  ┤   ╰─
+                temp (°C) for a period of 0:00:15
+                53.00  ┤
+                52.67  ┤
+                52.33  ┤
+                52.00  ┤
+                51.67  ┤      ╭─╮
+                51.33  ┤      │ │
+                51.00  ┤      │ │
+                50.67  ┤      │ ╰╮
+                50.33  ┤      │  │
+                50.00  ┤     ╭╯  │
+                49.67  ┤     │   │
+                49.33  ┤     │   │
+                49.00  ┼╮  ╭╮│   ╰─╮
+                48.67  ┤│  │││     │
+                48.33  ┤│  │││     │
+                48.00  ┤╰──╯╰╯     ╰───
 
-            /tmp/tmon-YYYYMMDD@HHhMMmSS-XXXXXXXX.txt
+                min: 48.0 °C
+                avg: 49.1 °C
+                max: 52.0 °C
+
+                raw: /tmp/tmon-YYYYMMDD@HHhMMmSS-XXXXXXXX.txt
+            ===================
+
+            $ %(prog)s -f -y 5 -x 5 bash -c 'sleep 6;stress -c 4 -t 3;sleep 6'
+            stress: info: [31055] dispatching hogs: 4 cpu, 0 io, 0 vm, 0 hdd
+            stress: info: [31055] successful run completed in 3s
+
+            ===================
+            Temp Monitor Report
+
+                temp (°F) for a period of 0:00:15
+                126.00  ┤
+                124.50  ┤  ╭╮
+                123.00  ┤ ╭╯│
+                121.50  ┤ │ ╰╮
+                120.00  ┼─╯  ╰
+
+                min: 120.2 °F
+                avg: 122.2 °F
+                max: 127.4 °F
+
+                raw: /tmp/tmon-YYYYMMDD@HHhMMmSS-XXXXXXXX.txt
             ===================
 
             $ %(prog)s -l 40 70 -x 10 -y 10 bash -c 'stress -c 4 -t 3; sleep 6'
-            stress: info: [19677] dispatching hogs: 4 cpu, 0 io, 0 vm, 0 hdd
-            stress: info: [19677] successful run completed in 3s
-
+            stress: info: [853] dispatching hogs: 4 cpu, 0 io, 0 vm, 0 hdd
+            stress: info: [853] successful run completed in 3s
 
             ===================
-            Temp Monitor Report:
+            Temp Monitor Report
 
-            Temp (°C) for a period of 0:00:09
-            70.00  ┤
-            66.67  ┤
-            63.33  ┤
-            60.00  ┤
-            56.67  ┼───╮
-            53.33  ┤   ╰─────
-            50.00  ┤
-            46.67  ┤
-            43.33  ┤
-            40.00  ┤
+                temp (°C) for a period of 0:00:09
+                70.00  ┤
+                66.67  ┤
+                63.33  ┤
+                60.00  ┤
+                56.67  ┤
+                53.33  ┤
+                50.00  ┼────╮   ╭
+                46.67  ┤    ╰───╯
+                43.33  ┤
+                40.00  ┤
 
-            /tmp/tmon-YYYYMMDD@HHhMMmSS-XXXXXXXX.txt
+                min: 49.0 °C
+                avg: 50.6 °C
+                max: 53.0 °C
+
+                raw: /tmp/tmon-YYYYMMDD@HHhMMmSS-XXXXXXXX.txt
             ===================
 
         copyright:
@@ -130,24 +151,44 @@ def parse_args():
     )
     parser.add_argument(
         "-v", "--version", required=False, action='store_true',
-        help="Shows %(prog)s version."
+        help="shows %(prog)s version"
     )
+    parser.add_argument(
+        "-f", "--fahrenheit", required=False, action='store_true',
+        help="shows temperatures in °F instead of °C (the default)"
+    )
+    parser.add_argument(
+        "-c", "--chart-only", required=False, action='store_true',
+        help=(
+            "only shows the temperature chart. Ignored if there is only one "
+            "data point"
+        )
+    )
+    parser.add_argument(
+        "-s", "--stats-only", required=False, action='store_true',
+        help="Only shows temperature stats"
+    )
+    parser.add_argument(
+        "-p", "--path-only", required=False, action='store_true',
+        help="only shows the path to raw data"
+    )
+
     parser.add_argument(
         "command", metavar='CMD', nargs=argparse.REMAINDER,
         help="Command args to run."
     )
     parser.add_argument(
         "-y", "--ysize", required=False, default=15, type=int,
-        help="Y-axis size in number terminal characters", action=AxisSizeAction
+        help="y-axis size in number terminal characters", action=AxisSizeAction
     )
     parser.add_argument(
         "-x", "--xsize", required=False, default=70, type=int,
-        help="X-axis size in number terminal characters", action=AxisSizeAction
+        help="x-axis size in number terminal characters", action=AxisSizeAction
     )
     parser.add_argument(
         "-l", "--ylim", nargs=2, required=False, metavar=('MIN', 'MAX'),
         type=float, help=(
-            "Y-axis view limits with min and max values. It is ignored if the "
+            "y-axis view limits with min and max values. It is ignored if the "
             "measured temperatures fall outside the specified range."
         )
     )
@@ -156,15 +197,27 @@ def parse_args():
 
 
 def main():
-    args = parse_args()
-    if args['version']:
+    kwargs = parse_args()
+    if kwargs['version']:
         print("Temperature Monitor -- tmon v0.3.8")
         return 0
-    t = tmon.TMon(
-        config={k: args[k] for k in args if k in ['ysize', 'xsize', 'ylim']}
+
+    if platform.system() != 'Linux':
+        print(textwrap.dedent("""
+            You seem to be running a non linux operating system. tmon is a
+            proud-to-be-linux-only tool, {} is not supported and will
+            probably never be. Please take the next step for a better tech
+            life and download a real operating system :)
+                https://mirrors.kernel.org/
+        """.format(platform.system())))
+        return 1
+
+    return tmon.run(
+        cmd=kwargs['command'],
+        xsize=kwargs['xsize'], ysize=kwargs['ysize'], ylim=kwargs['ylim'],
+        fahrenheit=kwargs['fahrenheit'], stats_only=kwargs['stats_only'],
+        chart_only=kwargs['chart_only'], path_only=kwargs['path_only']
     )
-    ret = t.run(args['command'])
-    return ret
 
 
 if __name__ == "__main__":
